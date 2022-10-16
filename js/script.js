a7d8e7cf0a16ee299dc65daba35e9f78f83c7944
Papil10n <nikept.co@gmail.com>
2022-10-16 16:39:21 +0300

    refactor script -Weather v0.2

--- js/script.js
+++ js/script.js
@@ -1,26 +1,14 @@
 // 60d465ff898de72b202b35030315ce9d     Api Key
 
-// need to refactor Dnipro js code
-
-let cityName = document.querySelector(".weather__city__name-name");
-let curTemp = document.querySelector(".weather__city__info-temp");
-let curWeather = document.querySelector(".info-cloud");
-
-let curDniproTemp = document.querySelector(".dnipro__temp");
-let cityDniproName = document.querySelector(".city-name");
-let humidityDnipro = document.querySelector(".info__humidity");
-let windDnipro = document.querySelector(".info__wind");
-let imgBlockDnipro = document.querySelector(".weather__img");
-
-let weatherBlockItem = document.querySelectorAll(".weather__timeBlock__article");
-
+// global Info
+const curWeather = document.querySelector(".info-cloud");
+const weatherBlockItem = document.querySelectorAll(".weather__timeBlock__article");
 
 // Location
 let BrowserLocation = window.location.href;
 let partLocation = BrowserLocation.substring(BrowserLocation.length - 12);
 let isMainPage = partLocation.includes("index.html") || partLocation.includes("ther_Site/");
 let isDnipro = partLocation.includes("dnipro.html");
-
 
 // set Current Date
 const week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -31,7 +19,6 @@ let curDay = new Date(Date.now());
 const transformDate = (index) => {
    return week[index];
 }
-
 if (isMainPage) {
    currDay.innerHTML = transformDate(curDay.getDay());
    currDate.innerHTML = `${curDay.getDate()}.${curDay.getMonth() + 1}.${curDay.getFullYear()}`;
@@ -40,58 +27,56 @@ if (isDnipro) {
    currDay.innerHTML = transformDate(curDay.getDay());
    currDate.innerHTML = `${curDay.getDate()}.${curDay.getMonth() + 1}.${curDay.getFullYear()}`;
 }
-
-
+// if (nextCity) {}
 
 
 // API request
 async function getCurrentWether() {
    let promise = await fetch('https://api.openweathermap.org/data/2.5/weather?lat=48.45&lon=35.04&appid=60d465ff898de72b202b35030315ce9d');
    let list = await promise.json();
-   // console.log(list);
 
-   let country = list.sys.country;
    let sity = list.name;
    let temp = normalizeTemp(list.main.temp);
-   let feelsLike = normalizeTemp(list.main.feels_like);
-   let maxTemp = normalizeTemp(list.main.temp_max);
-   let minTemp = normalizeTemp(list.main.temp_min);
    let humidity = list.main.humidity;
    let weather = list.weather[0];
    let mainDesc = weather.main;
-   let description = weather.description;
    let windSpeed = list.wind.speed;
-   let windDeg = list.wind.deg;
-   let weatherDate = document.querySelector(".weather__date");
-   let itemAddInfo = document.createElement("div");
-   itemAddInfo.classList.add("item__add__info");
-   itemAddInfo.innerHTML = weather.description;
-
 
    // Index
    if (isMainPage) {
+
+      const cityName = document.querySelector(".weather__city__name-name");
+      const curTemp = document.querySelector(".weather__city__info-temp");
+      const IndexBlockImg = document.querySelector(".weather__city__info-img");
+
       cityName.innerHTML = `${sity}, Ukraine`;
       curTemp.innerHTML = `${temp}°`;
       curWeather.innerHTML = mainDesc;
       curWeather.innerHTML = mainDesc;
+      showCurrentImg(IndexBlockImg, mainDesc);
    }
+
    // Dnipro
    if (isDnipro) {
+
+      const curDniproTemp = document.querySelector(".dnipro__temp");
+      const cityDniproName = document.querySelector(".city-name");
+      const humidityDnipro = document.querySelector(".info__humidity");
+      const windDnipro = document.querySelector(".info__wind");
+      const imgBlockDnipro = document.querySelector(".weather__img");
+      const weatherDate = document.querySelector(".weather__date");
+      const itemAddInfo = document.createElement("div");
+
       cityDniproName.innerHTML = `${sity}, Ukraine`;
       curDniproTemp.innerHTML = `${temp}°`;
       humidityDnipro.innerHTML = `${humidity}%`;
       curWeather.innerHTML = mainDesc;
       windDnipro.innerHTML = `${windSpeed.toFixed(1)} km/h`;
+      itemAddInfo.classList.add("item__add__info");
+      itemAddInfo.innerHTML = weather.description;
+
       weatherDate.append(itemAddInfo);
-      const imgCurrentWeather = document.createElement("img");
-      if (mainDesc == "Clear") {
-         imgCurrentWeather.setAttribute("src", "img/weatheriCO/sun.png");
-      } else if (mainDesc == "Clouds") {
-         imgCurrentWeather.setAttribute("src", "img/weatheriCO/cloud.png");
-      } else {
-         //####
-      }
-      imgBlockDnipro.append(imgCurrentWeather);
+      showCurrentImg(imgBlockDnipro, mainDesc);
 
       // Buttons "day"
       let day1 = document.querySelector(".day_1");
@@ -107,10 +92,6 @@ async function getCurrentWether() {
 }
 getCurrentWether();
 
-
-
-
-
 // get Day Forecast
 async function getForecastWeather() {
    let promise = await fetch('https://api.openweathermap.org/data/2.5/forecast?lat=48.46&lon=35.04&appid=60d465ff898de72b202b35030315ce9d');
@@ -118,123 +99,34 @@ async function getForecastWeather() {
       let list = await promise.json();
       let array = list.list;
 
-      console.log(array[0])
-
-
-      for (let i = 0; i < 8; i++) {
-
+      for (let i = 0; i < 5; i++) {
+         // Dnipro
          if (isDnipro) {
+
             let itemTime = (array[i].dt_txt).slice(10, array[i].dt_txt.length - 3);
             let itemWeather = array[i].weather[0].main;
             let itemTemp = (array[i].main.temp - 273.15).toFixed(0) + "°";
             let itemFeels = "ощущ. как: " + (array[i].main.feels_like - 273.15).toFixed(0) + "°";
+            let windDeg = array[i].wind.deg;
             let itemWind = "ветер: " + array[i].wind.speed.toFixed(1) + " км/ч";
 
             // create elem
             let newBlockItem = document.createElement("div");
             newBlockItem.classList.add("weather__timeBlock__item");
 
-            // create time
-            let divTime = document.createElement("div");
-            divTime.classList.add("weather__timeBlock__item__time");
-            let time = document.createElement("div");
-            time.classList.add("time");
-            divTime.append(time);
-            time.innerHTML = itemTime;
+            // set data
+            showTime(newBlockItem, itemTime);
+            showImg(newBlockItem, itemWeather, itemTime);
+            showWeatherInfo(newBlockItem, itemWeather);
+            showTemperature(newBlockItem, itemTemp);
+            showFeels(newBlockItem, itemFeels);
+            showWindImage(newBlockItem, windDeg);
+            showWind(newBlockItem, itemWind);
 
-            // create img 
-            let divImg = document.createElement("div");
-            divImg.classList.add("weather__timeBlock__item__img");
-            let imga = document.createElement("img");
-            imga.setAttribute("alt", "img");
-            let skyNow = array[i].weather[0].main;
-            // console.log(skyNow)
-
-            if (skyNow == "Clear") {
-               imga.setAttribute("src", "img/weatheriCO/sun.png");
-            } else if (skyNow == "Clouds") {
-               imga.setAttribute("src", "img/weatheriCO/cloud.png");
-            } else {
-               //####
-            }
-            divImg.append(imga);
-
-
-            // create weather
-            let divWeather = document.createElement("div");
-            divWeather.classList.add("weather__timeBlock__item__desc");
-            let weather = document.createElement("p");
-            weather.innerHTML = itemWeather;
-            divWeather.append(weather);
-
-            // create temp
-            let divTemp = document.createElement("div");
-            divTemp.classList.add("weather__timeBlock__item__temp");
-            let temp = document.createElement("p");
-            temp.innerHTML = itemTemp;
-            divTemp.append(temp);
-
-            // feels like 
-            let divFeels = document.createElement("div");
-            divFeels.classList.add("weather__timeBlock__item__feelsLike");
-            divFeels.innerHTML = itemFeels;
-
-            // wind img
-            let divWindImg = document.createElement("div");
-            divWindImg.classList.add("weather__timeBlock__item__windImg");
-            let windImg = document.createElement("img");
-            let windDeg = array[i].wind.deg;
-            windImg.setAttribute("alt", "img");
-            divWindImg.append(windImg);
-
-            if (windDeg > 315 || windDeg < 45) {
-               windImg.setAttribute("src", "./img/arrTop.png");
-            } else if (windDeg > 45 && windDeg < 135) {
-               windImg.setAttribute("src", "./img/arrRight.png");
-            } else if (windDeg > 135 && windDeg < 225) {
-               windImg.setAttribute("src", "./img/arrBottom.png");
-
-            } else if (windDeg > 225 && windDeg < 315) {
-               windImg.setAttribute("src", "./img/arrLeft.png");
-            }
-
-
-            // wind
-            let divWind = document.createElement("div");
-            divWind.classList.add("weather__timeBlock__item__wind");
-            let wind = document.createElement("p");
-            wind.innerHTML = itemWind;
-            divWind.append(wind);
-
-            // add to newBlock
-            newBlockItem.append(divTime);
-            newBlockItem.append(divImg);
-            newBlockItem.append(divWeather);
-            newBlockItem.append(divTemp);
-            newBlockItem.append(divFeels);
-            newBlockItem.append(divWindImg);
-            newBlockItem.append(divWind);
-
-            let fixTime = array[i].dt_txt.slice(11, array[i].dt_txt.length - 6)
-            if (itemTime.includes("09")) {
-               weatherBlockItem[0].append(newBlockItem);
-            }
-            if (itemTime.includes("12")) {
-               weatherBlockItem[1].append(newBlockItem);
-            }
-            if (itemTime.includes("15")) {
-               weatherBlockItem[2].append(newBlockItem);
-            }
-            if (itemTime.includes("18")) {
-               weatherBlockItem[3].append(newBlockItem);
-            }
-            if (itemTime.includes("21")) {
-               weatherBlockItem[4].append(newBlockItem);
-            }
+            //
+            weatherBlockItem[i].append(newBlockItem);
          }
-
       }
-
 
    } else {
       console.log("Ошибка HTTP: " + response.status);
@@ -243,26 +135,126 @@ async function getForecastWeather() {
 getForecastWeather();
 
 
-
-
-
-// get Sity lat and lon
+// get Sity lat and lon ( if need )
 async function getID() {
    let promise = await fetch('http://api.openweathermap.org/geo/1.0/direct?q=Dnipro&limit=5&appid=60d465ff898de72b202b35030315ce9d');
    let list = await promise.json();
 
-   // console.log(list);
 }
 
 
 
+// function"S
 
 const normalizeTemp = (temp) => {
    return (temp - 273.15).toFixed(0);
 }
 
+const showCurrentImg = (appendBlock, weatherInfo) => {
+   const imgCurImg = document.createElement("img");
+   if (weatherInfo == "Clear") {
+      imgCurImg.setAttribute("src", "img/weatheriCO/sun.png");
+   } else if (weatherInfo == "Clouds") {
+      imgCurImg.setAttribute("src", "img/weatheriCO/cloud.png");
+   } else {
+      //####
+   }
+   appendBlock.append(imgCurImg);
+}
 
+const showTime = (appendBlock, setTime) => {
+   const divTime = document.createElement("div");
+   divTime.classList.add("weather__timeBlock__item__time");
+   let time = document.createElement("div");
+   time.classList.add("time");
+   time.innerHTML = setTime;
+   divTime.append(time);
+   appendBlock.append(divTime);
+}
 
+//adding image cases
+const showImg = (appendBlock, setWeather, time) => {
+   const divImg = document.createElement("div");
+   divImg.classList.add("weather__timeBlock__item__img");
+   let imga = document.createElement("img");
+   imga.setAttribute("alt", "img");
+   let skyNow = setWeather;
 
+   switch (skyNow) {
+      case "Clear":
+         checkNigthTime(time) ?
+            imga.setAttribute("src", "img/weatheriCO/clearNigth.png")
+            : imga.setAttribute("src", "img/weatheriCO/sun.png");
 
+         break;
+      case "Clouds":
+         checkNigthTime(time) ?
+            imga.setAttribute("src", "img/weatheriCO/cloudNight.png")
+            : imga.setAttribute("src", "img/weatheriCO/cloud.png");
+         break;
+      // case #####:
+   }
 
+   divImg.append(imga);
+   appendBlock.append(divImg)
+};
+
+const showWeatherInfo = (appendBlock, itemWeatherInfo) => {
+   const divWeather = document.createElement("div");
+   divWeather.classList.add("weather__timeBlock__item__desc");
+   let weather = document.createElement("p");
+   weather.innerHTML = itemWeatherInfo;
+   divWeather.append(weather);
+   appendBlock.append(divWeather);
+}
+
+const showTemperature = (appendBlock, temperature) => {
+   const divTemp = document.createElement("div");
+   divTemp.classList.add("weather__timeBlock__item__temp");
+   let temp = document.createElement("p");
+   temp.innerHTML = temperature;
+   divTemp.append(temp);
+   appendBlock.append(divTemp);
+}
+
+const showFeels = (appendBlock, feelsLike) => {
+   let divFeels = document.createElement("div");
+   divFeels.classList.add("weather__timeBlock__item__feelsLike");
+   divFeels.innerHTML = feelsLike;
+   appendBlock.append(divFeels);
+}
+
+const showWindImage = (appendBlock, degrees) => {
+   const divWindImg = document.createElement("div");
+   divWindImg.classList.add("weather__timeBlock__item__windImg");
+   let windImg = document.createElement("img");
+   let windDeg = degrees;
+   windImg.setAttribute("alt", "img");
+   divWindImg.append(windImg);
+
+   if (windDeg > 315 || windDeg < 45) {
+      windImg.setAttribute("src", "./img/arrTop.png");
+   } else if (windDeg > 45 && windDeg < 135) {
+      windImg.setAttribute("src", "./img/arrRight.png");
+   } else if (windDeg > 135 && windDeg < 225) {
+      windImg.setAttribute("src", "./img/arrBottom.png");
+
+   } else if (windDeg > 225 && windDeg < 315) {
+      windImg.setAttribute("src", "./img/arrLeft.png");
+   }
+
+   appendBlock.append(divWindImg);
+}
+
+const showWind = (appendBlock, windInfo) => {
+   let divWind = document.createElement("div");
+   divWind.classList.add("weather__timeBlock__item__wind");
+   let wind = document.createElement("p");
+   wind.innerHTML = windInfo;
+   divWind.append(wind);
+   appendBlock.append(divWind);
+}
+
+const checkNigthTime = (nightTime) => {
+   return nightTime === " 21:00" || nightTime === " 00:00" || nightTime === " 03:00" ? true : false;
+}
